497c9c0d388d523acb9a98ade12b5661908e82b1
Michael Chris Trimbach <trimbamc@miamioh.edu>
2021-10-30 22:39:13 -0400

    Looking
    The player will now turn to face the mouse or the direction that the player moves the right stick.

--- Balancing Act/Assets/Scripts/Player.cs
+++ Balancing Act/Assets/Scripts/Player.cs
@@ -6,16 +6,22 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     private CharacterController controller = null;
+    private PlayerInput PI = null;
+    private Camera mainCamera = null;
 
     private float horizontal = 0;
     private float vertical = 0;
 
     [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private GameObject innerPlayer = null;
+    [SerializeField] private GameObject cursor = null;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = this.GetComponent<CharacterController>();
+        PI = this.GetComponent<PlayerInput>();
+        mainCamera = this.GetComponentInChildren<Camera>();
     }
 
     // Update is called once per frame
@@ -40,5 +46,27 @@ public class Player : MonoBehaviour
         Vector2 velocity = new Vector2(horizontal, vertical).normalized;
 
         controller.Move(velocity * moveSpeed * Time.deltaTime);
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Vector3 newLook = new Vector3();
+        Vector2 input = value.Get<Vector2>();
+
+        if (PI.currentControlScheme == "Keyboard&Mouse")
+        {
+            newLook = mainCamera.ScreenToWorldPoint(input);
+        }
+        else
+        {
+            cursor.SetActive(false);
+
+            newLook = new Vector3(this.transform.position.x + input.x, this.transform.position.y + input.y, this.transform.position.z);
+        }
+
+        float angle = Mathf.Atan2(newLook.x, newLook.y) % (2 * Mathf.PI);
+        angle = angle * -Mathf.Rad2Deg;
+
+        innerPlayer.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 }
